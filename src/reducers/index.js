eccca3ec8469a8f7a5167fde03cbc68cde151cad
alexrossetti <alexjoakland@gmail.com>
2019-01-11 13:37:40 +0000

    saved todos to browser cookies

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { ADD_TODO, DELETE_TODO, CLEAR_TODOS } from '../constants';
+import { bake_cookie, read_cookie } from 'sfcookies';
 
 const uuidv4 = require('uuid/v4');
 
@@ -18,16 +19,20 @@ const deleteTodo = (state = [], id) => {
 
 const todos = (state = [], action) => {
     let todos = null;
+    state = read_cookie('todos');
     
     switch(action.type){
         case ADD_TODO:
             todos = [...state, todo(action)];
+            bake_cookie('todos', todos);
             return todos;
         case DELETE_TODO:
             todos = deleteTodo(state, action.id);
+            bake_cookie('todos', todos);
             return todos;
         case CLEAR_TODOS:
             todos = [];
+            bake_cookie('todos', todos);
             return todos;
         default:
             return state;
