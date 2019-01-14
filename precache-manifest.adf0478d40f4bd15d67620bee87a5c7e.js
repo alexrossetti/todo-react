057fe7c817cd726ab3c6d6298f6ed2ac8a3ec976
alexrossetti <alexjoakland@gmail.com>
2019-01-14 13:02:00 +0000

    Updates

---
+++ precache-manifest.adf0478d40f4bd15d67620bee87a5c7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "330b348c81791189e348",
+    "url": "/todo-react/static/css/main.31d8df2d.chunk.css"
+  },
+  {
+    "revision": "330b348c81791189e348",
+    "url": "/todo-react/static/js/main.330b348c.chunk.js"
+  },
+  {
+    "revision": "0ed5f96a5915243c1a1f",
+    "url": "/todo-react/static/js/1.0ed5f96a.chunk.js"
+  },
+  {
+    "revision": "4ebad8e7a9f5566e1598",
+    "url": "/todo-react/static/js/runtime~main.4ebad8e7.js"
+  },
+  {
+    "revision": "795b17c6ff394202ba0c6bb58530ef82",
+    "url": "/todo-react/index.html"
+  }
+];
